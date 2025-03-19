b9247475e2da79460aa4ab88ec122ac4018a719c
Pradeep_Selladurai <152625492+Cyberpradeep@users.noreply.github.com>
2025-03-19 16:12:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var on_bt = document.getElementById("on");
+var off_bt = document.getElementById("off");
+var img = document.getElementById("bulb");
+function bulb_on(){
+    img.src="https://cdn.pixabay.com/photo/2014/04/03/10/41/light-311119_1280.png";
+}
+function bulb_off(){
+    img.src="https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_1280.png";
+}
